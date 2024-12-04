e3145113d382685864319712f2deb8f238404f90
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2024-12-04 18:32:10 +0200

    wifi: ath12k: move ATH12K_FLAG_REGISTERED handling to ath12k_mac_register()
    When hardware device group abstraction is introduced, a group abstraction is
    registered to mac80211 rather than a particular single device.  So we cannot
    set the device registered when the QMI firmware ready event is received, only
    after all the devices in group have received the event. To do that set and
    unset ATH12K_FLAG_REGISTERED flag inside ath12k_mac_register() and
    ath12k_mac_unregister() respectively.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Signed-off-by: Harshitha Prem <quic_hprem@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://patch.msgid.link/20241204163216.433795-2-kvalo@kernel.org
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -10843,6 +10843,8 @@ int ath12k_mac_register(struct ath12k_base *ab)
 			goto err;
 	}
 
+	set_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
+
 	return 0;
 
 err:
@@ -10861,6 +10863,8 @@ void ath12k_mac_unregister(struct ath12k_base *ab)
 {
 	struct ath12k_hw *ah;
 	int i;
+
+	clear_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
 
 	for (i = ath12k_get_num_hw(ab) - 1; i >= 0; i--) {
 		ah = ath12k_ab_to_ah(ab, i);
