166a9b1a8b86d19cbab9aceee0781c0bff0c2462
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2025-01-12 12:46:28 +0530

    wifi: ath12k: Refactor the ath12k_hw get helper function argument
    Currently, ath12k_hw is placed inside the ath12k_hw_group. However, the
    ath12k_hw get helper function takes the device handle and the index as
    parameters. Here, the index parameter is specific to the group handle.
    Therefore, change this helper function argument from the device handle
    to the group handle.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Acked-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Acked-by: Kalle Valo <kvalo@kernel.org>
    Link: https://patch.msgid.link/20250112071630.4059410-3-quic_periyasa@quicinc.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -11248,7 +11248,7 @@ int ath12k_mac_register(struct ath12k_hw_group *ag)
 	int ret;
 
 	for (i = 0; i < ath12k_get_num_hw(ab); i++) {
-		ah = ath12k_ab_to_ah(ab, i);
+		ah = ath12k_ag_to_ah(ag, i);
 
 		ret = ath12k_mac_hw_register(ah);
 		if (ret)
@@ -11261,7 +11261,7 @@ int ath12k_mac_register(struct ath12k_hw_group *ag)
 
 err:
 	for (i = i - 1; i >= 0; i--) {
-		ah = ath12k_ab_to_ah(ab, i);
+		ah = ath12k_ag_to_ah(ag, i);
 		if (!ah)
 			continue;
 
@@ -11280,7 +11280,7 @@ void ath12k_mac_unregister(struct ath12k_hw_group *ag)
 	clear_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
 
 	for (i = ath12k_get_num_hw(ab) - 1; i >= 0; i--) {
-		ah = ath12k_ab_to_ah(ab, i);
+		ah = ath12k_ag_to_ah(ag, i);
 		if (!ah)
 			continue;
 
@@ -11361,7 +11361,7 @@ void ath12k_mac_destroy(struct ath12k_hw_group *ag)
 	}
 
 	for (i = 0; i < ath12k_get_num_hw(ab); i++) {
-		ah = ath12k_ab_to_ah(ab, i);
+		ah = ath12k_ag_to_ah(ag, i);
 		if (!ah)
 			continue;
 
@@ -11442,7 +11442,7 @@ int ath12k_mac_allocate(struct ath12k_hw_group *ag)
 
 err:
 	for (i = i - 1; i >= 0; i--) {
-		ah = ath12k_ab_to_ah(ab, i);
+		ah = ath12k_ag_to_ah(ag, i);
 		if (!ah)
 			continue;
 
